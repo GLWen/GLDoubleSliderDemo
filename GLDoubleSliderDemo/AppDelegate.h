b72ab6dcd5a202353a04da95655ffec4c95cb5ad
wenguoli <15160333779@163.com>
2016-09-20 15:43:02 +0800

    Initial Commit

---
+++ GLDoubleSliderDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GLDoubleSliderDemo
+//
+//  Created by 温国力 on 16/9/20.
+//  Copyright © 2016年 wenguoli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GLDoubleSliderDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GLDoubleSliderDemo
+//
+//  Created by 温国力 on 16/9/20.
+//  Copyright © 2016年 wenguoli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
